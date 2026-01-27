69d6bd9a471e9c39829ebd29210238de8328ed5e
song_eun <dmswo546@gmail.com>
2026-01-27 15:07:29 +0900

    fix(storybook): addon-essentials 추가 및 ajv 의존성 수정

--- frontend/.storybook/main.ts
+++ frontend/.storybook/main.ts
@@ -4,10 +4,10 @@ const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
 
   addons: [
+    "@storybook/addon-essentials",
     "@storybook/addon-links",
     "@storybook/addon-themes",
     "@chromatic-com/storybook",
-    "@storybook/addon-docs",
   ],
 
   framework: {
