20dea58714aaec3988d2193842a24c7d2a4a6f0f
song_eun <dmswo546@gmail.com>
2026-01-31 07:13:33 +0900

    feat(frontend): LeadsRepository 추가

--- frontend/src/repositories/index.ts
+++ frontend/src/repositories/index.ts
@@ -1,2 +1,4 @@
 export { AIdolRepository } from "./AIdolRepository";
 export { CompanionRepository } from "./CompanionRepository";
+export { LeadsRepository } from "./LeadsRepository";
+export type { LeadRequest, LeadResponse } from "./LeadsRepository";
