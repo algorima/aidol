239f57fbf0df21d28bade52da0a5332b05cf074a
zhwltlr <vyoungv7340@naver.com>
2026-02-02 22:10:47 +0900

    refactor(frontend): mock 코드 정리 및 삭제
    - MockCompanionRepository.ts 삭제
    - mocks/companions.ts 삭제
    - repositories/index.ts에서 mock export 제거
    - CastingPage.stories.tsx에서 인라인 샘플 데이터로 대체

--- frontend/src/repositories/index.ts
+++ frontend/src/repositories/index.ts
@@ -1,3 +1,2 @@
 export { AIdolRepository } from "./AIdolRepository";
 export { CompanionRepository } from "./CompanionRepository";
-export { getMockCompanionRepository } from "./MockCompanionRepository";
