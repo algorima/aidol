9f424e071db1a242a2f0bb11042e02b926e45392
song_eun <dmswo546@gmail.com>
2026-02-04 04:28:20 +0900

    fix(frontend): casting 페이지 Header children 방식으로 변경

--- frontend/src/stories/CastingPage.stories.tsx
+++ frontend/src/stories/CastingPage.stories.tsx
@@ -33,15 +33,12 @@ function CastingPageFull() {
 
   return (
     <div className="max-w-mobile mx-auto flex min-h-screen flex-col">
-      <Header
-        title="캐스팅"
-        rightContent={
-          <button type="button" className="btn btn-primary gap-2.5">
-            신규 멤버
-            <PlusIcon className="size-4" />
-          </button>
-        }
-      />
+      <Header title="캐스팅">
+        <button type="button" className="btn btn-primary gap-2.5">
+          신규 멤버
+          <PlusIcon className="size-4" />
+        </button>
+      </Header>
 
       <div className="flex flex-col gap-6 px-6 py-4">
         <CastingInfoBanner />
