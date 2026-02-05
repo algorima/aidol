0e99ad4f731c20d6493f6628cd367f41d87bf69b
pjh <pjhdye423@gmail.com>
2026-02-06 02:45:51 +0900

    refactor(services): 친밀도 변환 로직 서비스로 분리

--- backend/aidol/services/__init__.py
+++ backend/aidol/services/__init__.py
@@ -2,10 +2,16 @@
 AIdol services
 """
 
+from aidol.services.companion_relationship_service import (
+    IntimacyType,
+    get_intimacy_type,
+)
 from aidol.services.image_generation_service import ImageGenerationService
 from aidol.services.response_generation_service import ResponseGenerationService
 
 __all__ = [
     "ImageGenerationService",
+    "IntimacyType",
     "ResponseGenerationService",
+    "get_intimacy_type",
 ]
