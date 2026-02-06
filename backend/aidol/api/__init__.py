a1dce57c64029cd0da9cca6d5029cc9265d3700e
pjh <pjhdye423@gmail.com>
2026-02-06 15:53:21 +0900

    feat(api): companion-relationships CRUD 엔드포인트 추가

--- backend/aidol/api/__init__.py
+++ backend/aidol/api/__init__.py
@@ -5,16 +5,23 @@ AIdol API routers
 from aidol.api.aidol import AIdolRouter, create_aidol_router
 from aidol.api.chatroom import ChatroomRouter, create_chatroom_router
 from aidol.api.companion import CompanionRouter, create_companion_router
+from aidol.api.companion_relationship import (
+    CompanionRelationshipRouter,
+    create_companion_relationship_router,
+)
 from aidol.api.highlight import HighlightRouter, create_highlight_router
 
 __all__ = [
     "AIdolRouter",
     "ChatroomRouter",
+    "CompanionRelationshipRouter",
     "CompanionRouter",
     "HighlightRouter",
     "create_aidol_router",
     "create_chatroom_router",
+    "create_companion_relationship_router",
     "create_companion_router",
     "create_highlight_router",
 ]
+
 
