516b089e23918379cb7369a4e0e51b10903caa4a
pjh <pjhdye423@gmail.com>
2026-02-06 15:24:45 +0900

    feat(api): GET /aidol-highlights 및 messages 엔드포인트 추가

--- backend/aidol/api/__init__.py
+++ backend/aidol/api/__init__.py
@@ -5,12 +5,16 @@ AIdol API routers
 from aidol.api.aidol import AIdolRouter, create_aidol_router
 from aidol.api.chatroom import ChatroomRouter, create_chatroom_router
 from aidol.api.companion import CompanionRouter, create_companion_router
+from aidol.api.highlight import HighlightRouter, create_highlight_router
 
 __all__ = [
     "AIdolRouter",
     "ChatroomRouter",
     "CompanionRouter",
+    "HighlightRouter",
     "create_aidol_router",
     "create_chatroom_router",
     "create_companion_router",
+    "create_highlight_router",
 ]
+
