841f2cdaec6de272064b3c6362c438f6f8e5f793
zhwltlr <vyoungv7340@naver.com>
2026-02-12 15:17:46 +0900

    refactor(frontend): 기존 페이지 수정 및 [aidolId]/home 삭제
    - casting, my-group 페이지에서 BottomNavigation aidolId prop 제거
    - detail 페이지 뒤로가기 URL을 /aidols/home으로 변경
    - [aidolId]/home 페이지 삭제 (/aidols/home으로 대체 완료)

--- frontend/src/app/[lang]/aidols/my-group/[aidolId]/page.tsx
+++ frontend/src/app/[lang]/aidols/my-group/[aidolId]/page.tsx
@@ -222,7 +222,7 @@ export default function GroupPage() {
         </div>
       </div>
 
-      <BottomNavigation aidolId={params.aidolId} lang={params.lang} />
+      <BottomNavigation lang={params.lang} />
 
       <HighlightDetailModal
         isOpen={isModalOpen}
